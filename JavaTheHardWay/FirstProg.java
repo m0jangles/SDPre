08665eb1c15e2c4aa7f1d992309b55f0bfbc72ec
Amber Moreno <amber.e.moreno@gmail.com>
2018-06-07 22:17:06 -0600

    Getting Started

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main ( String[] args) {
+    System.out.println ( "I, Amber Moreno, am determined to learn how to code");
+    System.out.println ( "Today's date is June 7, 2018" );
+  }
+}
---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,15 @@
+public class GasolineReceipt {
+  public static void main ( String [] args) {
+    System.out.println( "+------------------------+");
+    System.out.println( "|                        |");
+    System.out.println( "|      CORNER STORE      |");
+    System.out.println( "| 2018-06-07  10:05PM    |");
+    System.out.println( "|                        |");
+    System.out.println( "| Gallons:       10.870  |");
+    System.out.println( "| Price/gallon: $ 2.83  |");
+    System.out.println( "|                        |");
+    System.out.println( "| Fuel total:  $ 30.76   |");
+    System.out.println( "|                        |");
+    System.out.println( "+------------------------+");
+  }
+}
