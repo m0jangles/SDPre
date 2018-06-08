8a3ec5adca62cd1e940930ad840eb2bf4a720690
Amber Moreno <amber.e.moreno@gmail.com>
2018-06-07 23:41:25 -0600

    PrintingChoicesEscapes

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,10 @@
+public class PrintingChoicesEscapes {
+  public static void main( String[] args) {
+    // This exercise demonstrates escape sequences & comments (like these)!
+    System.out.println( "Learn\tJava\n\tthe\nHard\tWay\n" + "\tLearn Java the \"Hard\" Way!\n" + "Hello\n" + "Jello\by\n" + "Hard to believe, eh?\n" + "Surprised? /* abcde */ Or what did you expect?\n" + "\\ // -=- \\ //\n" + "\\\\ \\\\\\ \\\\\\\\\n" + "I hope you understand \" escape sequences\" now. \n" );
+
+
+
+    //code will not compile if you add block comment in the middle of println
+  }
+}
