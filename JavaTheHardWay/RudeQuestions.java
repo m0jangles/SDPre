cbe505120b0d3b5159a4594242e160ee9a5caba7
Amber Moreno <amber.e.moreno@gmail.com>
2018-06-09 17:42:45 -0600

    RudeQuestions

---
+++ JavaTheHardWay/RudeQuestions.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class RudeQuestions{
+  public static void main (String[] args ) {
+    String name, eggs;
+    int age;
+    double weight, income;
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print( "Hello. What is your name? ");
+    name = keyboard.next();
+
+    System.out.print( "Hi, " + name + "! How old are you? ");
+    age = keyboard.nextInt();
+
+    System.out.println( "So you're " + age + ", eh? That's not very old.");
+    System.out.print( "How much do you weigh, " + name + "? ");
+    weight = keyboard.nextDouble();
+
+    System.out.println(weight + "! Better keep that quiet!!");
+    System.out.print("Finally, what's your income, " + name + "? ");
+    income = keyboard.nextDouble();
+
+    System.out.print( "Hopefully that is " + income + " per hour");
+    System.out.println( " and not per year!");
+    System.out.print( "Well, thanks for answering my rude questions, ");
+    System.out.println( name + ".");
+
+    System.out.print( "Do you watch GoT? ");
+    eggs = keyboard.next();
+
+    System.out.println( eggs + " ?" + " I can't wait for the next season.");
+
+    //No, the program does not blow up when you enter an integer when the program
+    //is expecting a double because floating point variables can hold integers
+    //No, the program does not blow up when you enter a numberic value when import junit.framework.TestCase;
+    //is expecting a string because a strin input can be a numberic value
+    //If I enter an import statement, for example, the import statement for the
+    //scanner library, the program blows up after the first statement because this is
+    //part of the code and it probably confuses the program
+
+  }
+}
