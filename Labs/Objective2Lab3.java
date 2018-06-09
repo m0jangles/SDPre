42251625dc6eb59aa6b6dadb0b27af3c575e17f2
Amber Moreno <amber.e.moreno@gmail.com>
2018-06-09 13:03:35 -0600

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args ){
+  String food1, food2, food3;
+
+  food1 = "tacos";
+  food2 = "hot wings";
+  food3 = "more tacos";
+
+  System.out.println("My Favorite Food Are: ");
+  System.out.println(food1);
+  System.out.println(food2);
+  System.out.println(food3);
+  }
+}
