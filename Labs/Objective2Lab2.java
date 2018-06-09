bd9848580a55b1af7664843932a88e52dfc9d3e8
Amber Moreno <amber.e.moreno@gmail.com>
2018-06-09 11:10:07 -0600

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String [] args ){
+
+  String name = "Amber Moreno";
+  int age = 33;
+  String state = "California";
+
+  System.out.println( "Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
