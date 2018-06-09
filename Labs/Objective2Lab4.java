9cdf70d9fc3c8c12055ba060b77ac9fb12b3c56b
Amber Moreno <amber.e.moreno@gmail.com>
2018-06-09 13:11:56 -0600

    Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,14 @@
+public class Objective2Lab4{
+  public static void main(String [] args ){
+    double num1, num2, num3, average;
+
+    num1 = 5.23;
+    num2 = 3.32;
+    num3 = 4.25;
+
+    average = (num1+num2+num3)/3;
+    System.out.println(average);
+
+
+  }
+}
