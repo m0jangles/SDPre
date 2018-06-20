ed1ab768138e6cbf5f33176b6b379e961571dbb6
Amber Moreno <amber.e.moreno@gmail.com>
2018-06-20 12:23:55 -0600

    Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,11 @@
+public class Objective6Lab4{
+  public static void main(String[] args ){
+    int sum = 0, count = 0;
+
+    while (count < 20){
+      count ++;
+      sum += count;
+    }
+    System.out.println(sum);
+  }
+}
